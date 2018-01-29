f98a703f2b465e909661e47292232dffefb23dc3
zhangz12 <zhangz12@rose-hulman.edu>
2018-01-29 06:51:13 -0500

    m2

--- src/m2_mutating_returning_lists.py
+++ src/m2_mutating_returning_lists.py
@@ -5,8 +5,8 @@ This problem provides practice at:
   ***  MUTATING  and  RETURNING-NEW  LISTS.  ***
 
 Authors: David Mutchler, Valerie Galluzzi, Mark Hays, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Zheming Zhang.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 ########################################################################
 # Students:
@@ -37,7 +37,7 @@ def main():
 def run_test_doubler():
     """ Tests the    doubler    function. """
     # ------------------------------------------------------------------
-    # TODO: 2. Implement this TEST function.
+    # DONE: 2. Implement this TEST function.
     #   It TESTS the  doubler  function defined below.
     #   Include at least ** 1 ** ADDITIONAL test beyond those we wrote.
     #
@@ -83,6 +83,28 @@ def run_test_doubler():
     # TO DO 2 (continued): Add your ADDITIONAL test(s) here:
     # ------------------------------------------------------------------
 
+    # Test 2:
+    arg3 = [10, 2, -100, 20, 4, 9]
+    arg4 = [2, 5, 158]
+    correct_arg3_after = [20, 4, -200, 40, 8, 18]
+    correct_arg4_after = [4, 10, 316]
+    expected = [4, 10, 316]
+
+    print()
+    print('BEFORE the function call:')
+    print('  Argument 1 is:', arg3)
+    print('  Argument 2 is:', arg4)
+
+    answer = doubler(arg3, arg4)
+
+    print('AFTER the function call:')
+    print('  Argument 1 is:       ', arg3)
+    print('  Argument 1 should be:', correct_arg3_after)
+    print('  Argument 2 is:       ', arg4)
+    print('  Argument 2 should be:', correct_arg4_after)
+    print('The returned value is:       ', answer)
+    print('The returned value should be:', expected)
+
 
 def doubler(list1, list2):
     """
@@ -102,7 +124,7 @@ def doubler(list1, list2):
         :type list2: list of int
     """
     # ------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # DONE: 3. Implement and test this function.
     #   Note that you should write its TEST function first (above).
     # ------------------------------------------------------------------
     # ------------------------------------------------------------------
@@ -110,6 +132,14 @@ def doubler(list1, list2):
     #    DIFFICULTY:      4
     #    TIME ESTIMATE:   5 minutes.
     # ------------------------------------------------------------------
+
+    d = []
+    for k in range(len(list1)):
+        list1[k] = 2 * list1[k]
+    for l in range(len(list2)):
+        d += [2 * list2[l]]
+    return d
+
 
 
 # ----------------------------------------------------------------------
